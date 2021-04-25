a2b7c6a60a4f876b3a4a60deb97a4b372a0efaef
yohanfaj <yohan.fajerman@efrei.net>
2021-04-25 16:02:55 +0200

    compiler works

--- main.c
+++ main.c
@@ -1,9 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #include "conio.h"
-#include "rubiks.h"
+#include "rubiks.c"
 #define FACE 6
 #define SIZE 3
 
@@ -12,7 +11,7 @@ void main(void)
 {
     int ***rubiks;
     create_rubiks(&rubiks);
-    fill_rubiks(rubiks);
+    init_rubiks(rubiks);
     printf("Here is the rubiks cube: \n");
     display_rubiks(rubiks);
     free_rubiks(rubiks);
--- rubiks.c
+++ rubiks.c
@@ -1,7 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <conio.h>
 #include "conio.h"
 #include "rubiks.h"
 #define FACE 6
