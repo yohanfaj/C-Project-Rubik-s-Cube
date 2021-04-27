8ca77f73ee539b9314c3a27384ea106b08bb590f
yohanfaj <yohan.fajerman@efrei.net>
2021-04-27 18:11:13 +0200

    main: 1st interface

--- main.c
+++ main.c
@@ -9,18 +9,40 @@
 
 void main(void)
 {
-    int i,j,k;
+    int i,j,k, op;
     char ***rubiks = create_rubiks();
-    printf("Here is a normal rubiks cube: \n");
-    init_rubiks(rubiks);
-    display_rubiks(rubiks);
-    printf("Here is a blank rubiks cube: \n");
-    blank_rubiks(rubiks);
-    display_rubiks(rubiks);
-    printf("Please fill the cube with the following letters: R for RED, B for BLUE, G for GREEN, Y for YELLOW, O for ORANGE or W for WHITE: \n");
-    fill_face(rubiks);
-    printf("Here is your custom rubiks cube: \n");
-    display_rubiks(rubiks);
+    printf("Welcome to the RUBIK'S CUBE SIMULATOR ! \n");
+    do
+    {
+        printf("What do you want to do ? \nPress 1 to display a blank cube, 2 to initialize it or 3 to custom it: ");
+        scanf("%d", &op);
+    } while (op <= 0 && op >3);
+    printf("\n");
+
+    switch (op)
+    {
+    case 1:
+        printf("Here is your blank rubiks cube: \n");
+        blank_rubiks(rubiks);
+        display_rubiks(rubiks);
+        break;
+    
+    case 2: 
+        printf("Here is a normal rubiks cube: \n");
+        init_rubiks(rubiks);
+        display_rubiks(rubiks);
+        break;
+
+    case 3:
+        printf("Please fill the cube with the following letters: R for RED, B for BLUE, G for GREEN, Y for YELLOW, O for ORANGE or W for WHITE: \n");
+        fill_face(rubiks);
+        printf("Here is your custom rubiks cube: \n");
+        display_rubiks(rubiks);
+    default:
+        break;
+    }
+
     free_rubiks(rubiks);
+    printf("Thank you for using the simulator. Have a nice day ! ");
 }
 
