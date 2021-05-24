fd66ef3cfe4af26ab767ff0d79d23248a77bb2c9
Okajima94 <adbarbier.2002@gmail.com>
2021-05-24 18:08:15 +0200

    yellow cross beginning

--- rubiks.h
+++ rubiks.h
@@ -87,6 +87,14 @@ int left_move_check(char ***rubiks);
 int right_move_check(char ***rubiks);
 int bad_orientation(char ***rubiks);
 int second_crown_check(char ***rubiks);
+void last_crown(char ***rubiks);
+int yellow_cross_check(char ***rubiks);
+int perfect_yellow_cross(char ***rubiks);
+int yellow_cross_facing_edges(char ***rubiks);
+int yellow_cross_side_by_side(char ***rubiks);
+int single_case_yellow(char ***rubiks);
+int L_shape(char ***rubiks);
+int barr(char ***rubiks);
 
 /* TEXT COLOR FUNCTION */
 void text_color(int color);
